3854c3ab6baa0a8b1cff63c20ea11bb55f6b5d1e
theov <ukipr@student.kit.edu>
2025-07-01 16:48:13 +0200

    QRCodeReader.py as object

--- backend/QRCodeReader.py
+++ backend/QRCodeReader.py
@@ -1,39 +1,41 @@
 import cv2
 from pyzbar import pyzbar
 
-cap = cv2.VideoCapture(0)
+class QRCodeReader:
 
-cap.set(cv2.CAP_PROP_FRAME_WIDTH, 1920)
-cap.set(cv2.CAP_PROP_FRAME_HEIGHT, 1080)
+    def __init__(self):
+        self.cap = cv2.VideoCapture(0)
+        self.cap.set(cv2.CAP_PROP_FRAME_WIDTH, 1920)
+        self.cap.set(cv2.CAP_PROP_FRAME_HEIGHT, 1080)
 
-print("fertig")
-
-def make_picture():
-    ret, picture = cap.read()
-    if not ret:
-        print("No picture received.")
-    else:
-        filename = f"pictures/qrcodes.jpg"
-        cv2.imwrite(filename, picture)
-        print(f"picture saved: {filename}")
+    def __make_picture(self):
+        ret, picture = self.cap.read()
+        if not ret:
+            print("No picture received.")
+        else:
+            filename = f"pictures/qrcodes.jpg"
+            cv2.imwrite(filename, picture)
+            print(f"picture saved: {filename}")
 
 
-def read_qr_codes(picture_path):
+    def __read_qr_codes(self, picture_path):
 
-    picture = cv2.imread(picture_path)
-    if picture is None:
-        return "Path invalid"
+        picture = cv2.imread(picture_path)
+        if picture is None:
+            return "Path invalid"
 
-    qr_codes = pyzbar.decode(picture)
+        qr_codes = pyzbar.decode(picture)
 
-    results = []
-    for qr_code in qr_codes:
-        data = qr_code.data.decode('utf-8')
-        coordinates = [(point.x, point.y) for point in qr_code.polygon]
-        results.append({'data': data, 'coordinates': coordinates})
+        results = []
+        for qr_code in qr_codes:
+            data = qr_code.data.decode('utf-8')
+            coordinates = [(point.x, point.y) for point in qr_code.polygon]
+            results.append({'data': data, 'coordinates': coordinates})
 
-    return results
+        return results
 
-make_picture()
-final_results = read_qr_codes("pictures/qrcodes.jpg")
-print(final_results)
+    def give_box_qr_codes_and_positions(self):
+        self.__make_picture()
+        final_results = self.__read_qr_codes("pictures/qrcodes.jpg")
+        return final_results
+
---
+++ backend/Test.py
@@ -0,0 +1,5 @@
+from backend.QRCodeReader import QRCodeReader
+
+qr_code_reader = QRCodeReader()
+
+print(qr_code_reader.give_box_qr_codes_and_positions())
